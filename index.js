6d9dc45f04b518b8f236e03bb16d44444e5097ea
Mohammad Ibrahim <ibnabdullah1357@gmail.com>
2023-12-01 18:05:24 +0600

    reservation apis created

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 
 const app = express()
 const port = process.env.PORT || 5000
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 app.use(express.json())
 app.use(cors())
 
@@ -26,10 +26,12 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const usersCollection = client.db('diagnostic-center').collection('users')
         const testCollection = client.db('diagnostic-center').collection('tests')
+        const bannerCollection = client.db('diagnostic-center').collection('banners')
+        const reservationCollection = client.db('diagnostic-center').collection('reservations')
         /* User Related API */
         app.post("/users", async (req, res) => {
             const user = req.body;
@@ -100,17 +102,26 @@ async function run() {
             const tests = await testCollection.find().toArray()
             res.send(tests)
         })
+        app.get("/test/:id", async (req, res) => {
+            // const email = req.query.email;
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const tests = await testCollection.findOne(query)
+            res.send(tests)
+        })
         app.put("/tests/:id", async (req, res) => {
             const id = req.params.id;
             console.log(id);
             const data = req.body;
-            const { availableSlots, testDate, testDescription, testImage, testName, _id } = data
+            const { availableSlots, testDate, testDescription, testImage, testName, _id, testFee } = data
             // console.log("id", id, data);
             const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
             const updatedTest = {
                 $set: {
-                    availableSlots, testDate, testDescription, testImage, testName
+                    availableSlots, testDate, testDescription, testImage, testName, testFee
 
                 },
             };
@@ -131,10 +142,147 @@ async function run() {
             console.log(result);
             res.send(result);
         });
+        app.put("/confirm-payment", async (req, res) => {
+            const { testId } = req.body;
+
+            try {
+                // Assuming 'testId' is the MongoDB ObjectId of the test you want to update
+                const filter = { _id: new ObjectId(testId) };
+
+                // Decrement availableSlots by 1
+                const update = { $inc: { availableSlots: -1 } };
+                const options = { upsert: false };
+
+                const result = await testCollection.updateOne(filter, update);
+
+                if (result.modifiedCount === 1) {
+                    // If the update was successful, send a success response
+                    res.send({ success: true, message: "Booking successful!" });
+                } else {
+                    // If the update failed, send an error response
+                    res.status(500).send({ success: false, message: "Failed to update available slots." });
+                }
+            } catch (error) {
+                console.error("Error updating available slots:", error);
+                res.status(500).send({ success: false, message: "Internal server error." });
+            }
+        });
+
+
+        /* Banner Related API */
+        app.post("/banner", async (req, res) => {
+            const banner = req.body;
+            //   console.log(user);
+            const result = await bannerCollection.insertOne(banner);
+            console.log(result);
+            res.send(result);
+        });
+        app.get("/banners", async (req, res) => {
+            // const email = req.query.email;
+            const banners = await bannerCollection.find().toArray()
+            res.send(banners)
+        })
+
+        app.delete("/banner/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log("delete", id);
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await bannerCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        });
+        /* Update Activit Status of Banner */
+        app.put("/banners/:id", async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            // console.log(id);
+
+
+            const { isActive } = data;
+            const filter = { _id: new ObjectId(id) };
+            const updateData = {
+                $set: {
+                    isActive: true,
+                },
+            };
+
+            const result = await bannerCollection.updateOne(filter, updateData);
+            res.send(result)
+
+        });
+        /* reset all activit status */
+        app.put("/banners", async (req, res) => {
+            const resetResult = await bannerCollection.updateMany({}, { $set: { isActive: false } });
+            res.send(resetResult)
+        });
+        /* Reservations API */
+        app.post("/reservations", async (req, res) => {
+            const reservation = req.body;
+            //   console.log(user);
+            const result = await reservationCollection.insertOne(reservation);
+            console.log(result);
+            res.send(result);
+        });
+        app.get("/reservations", async (req, res) => {
+            // const email = req.query.email;
+            const tests = await reservationCollection.find().toArray()
+            res.send(tests)
+        })
+        app.patch("/reservation/:id", async (req, res) => {
+            const userId = req.params.id;
+            const { report, reportStatus } = req.body;
+            // console.log(selectedUserType);
+            const filter = { _id: new ObjectId(userId) };
+            try {
+
+                const update = { $set: { reportStatus: reportStatus, report: report } };
+
+                const result = await reservationCollection.updateOne(filter, update);
+
+                res.send(result)
+            } catch (error) {
+                console.error(error);
+            }
+        });
+        app.delete("/reservation/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log("delete", id);
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await reservationCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        });
+
+
+
+        /* Stripe Api */
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100)
+            if (isNaN(amount)) {
+                // Handle the case where price is NaN
+                console.error('Invalid price value');
+                // Add appropriate error response or fallback value
+            } else {
+                // Proceed with creating payment intent
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amount,
+                    currency: 'usd',
+                    payment_method_types: ['card']
+                });
+                res.send({
+                    clientSecret: paymentIntent.client_secret
+                });
+            }
 
 
 
 
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
